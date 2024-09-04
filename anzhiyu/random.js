9514f6c815a8bed8965ffeba49d207113b54ab8c
C+V <2300013554@stu.pku.edu.cn>
2024-09-04 15:35:04 +0800

    Site updated: 2024-09-04 15:35:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/30/CSAPP/","2024/08/25/hello-world/","2024/09/04/鸡汤/"];function toRandomPost(){
+var posts=["2024/08/30/CSAPP/","2024/08/25/hello-world/","2024/09/04/title/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
