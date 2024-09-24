10ccb581a4281a065989c94aa92a8a5148dc1d81
C+V <2300013554@stu.pku.edu.cn>
2024-09-24 21:35:07 +0800

    Site updated: 2024-09-24 21:35:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/30/CSAPP/","2024/09/20/algorithm/","2024/08/25/hello-world/","2024/09/12/ics第一周-信息的表示和处理/","2024/09/23/ics第三周-程序的机器级表示/","2024/09/14/ics第二周-信息的表示和处理续/","2024/09/20/ics第二周-程序的机器级表示/","2024/09/24/test/","2024/09/04/title/"];function toRandomPost(){
+var posts=["2024/08/30/CSAPP/","2024/08/25/hello-world/","2024/09/20/algorithm/","2024/09/12/ics第一周-信息的表示和处理/","2024/09/23/ics第三周-程序的机器级表示/","2024/09/14/ics第二周-信息的表示和处理续/","2024/09/20/ics第二周-程序的机器级表示/","2024/09/24/test/","2024/09/04/title/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
