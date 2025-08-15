2c65c42c9acef09386dc15fd55a58b7ae1b49583
C+V <2300013554@stu.pku.edu.cn>
2025-08-15 17:35:59 +0800

    Site updated: 2025-08-15 17:35:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/7c8b4f72/","posts/ded8437/","posts/d253f8ca/","posts/5cca8783/","posts/cc548c89/","posts/4145f9d2/","posts/ee040603/","posts/ef66d2a/","posts/dd893702/","posts/c7b885a3/","posts/147acb51/"];function toRandomPost(){
+var posts=["posts/7c8b4f72/","posts/ded8437/","posts/5cca8783/","posts/d253f8ca/","posts/4145f9d2/","posts/cc548c89/","posts/ee040603/","posts/147acb51/","posts/ef66d2a/","posts/c7b885a3/","posts/dd893702/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
